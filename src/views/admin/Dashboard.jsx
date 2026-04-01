c1d512f55ff47c0d1f0143f64a304b19f5004853
Elvina <cat250070@gmail.tw>
2026-04-01 13:33:34 +0800

    移除後台登入資訊

--- src/views/admin/Dashboard.jsx
+++ src/views/admin/Dashboard.jsx
@@ -15,8 +15,8 @@ const Dashboard = () => {
   } = useForm({
     mode: "onChange",
     defaultValues: {
-      username: "sweetchen709+1@gmail.com",
-      password: "ononcake666",
+      username: "",
+      password: "",
     },
   });
 
